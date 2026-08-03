a542f903fa3cb41531036394fb856f5edde58c4f
yadav-adi45 <aytbkpma@gmail.com>
2026-08-03 14:33:39 +0530

    implement routes in navbar

--- react-retest/src/App.jsx
+++ react-retest/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-
+import { BrowerRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
+import Form from './components/Form/Form'
 import './App.css'
 
 function App() {
@@ -7,7 +8,13 @@ function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<Form />} />
+          
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- react-retest/src/components/Navbar/Navbar.jsx
+++ react-retest/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import './Navbar.css'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+        <div className="logo"></div>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/form">Form</Link>
+        </div>
     </div>
   )
 }
