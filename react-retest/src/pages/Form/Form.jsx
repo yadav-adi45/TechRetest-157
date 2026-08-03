19d3011182af701cebbb6ac3c624171b5734ccec
yadav-adi45 <aytbkpma@gmail.com>
2026-08-03 15:30:38 +0530

    Added Questions

--- react-retest/src/pages/Form/Form.jsx
+++ react-retest/src/pages/Form/Form.jsx
@@ -1,4 +1,47 @@
 const Form = () => {
+  const Questions = [
+    {
+      "questionNo": 1,
+      "question": 'What is React?'
+    },
+    {
+      "questionNo": 2,
+      "question": 'What is JSX?'
+    },
+    {
+      "questionNo": 3,
+      "question": 'What is a Component?'
+    },
+    {
+      "questionNo": 4,
+      "question": 'What is Props?'
+    },
+    {
+      "questionNo": 5,
+      "question": 'What is useState?'
+    },
+    {
+      "questionNo": 6,
+      "question": 'What is Purpose of React Router?'
+    },
+    {
+      "questionNo": 7,
+      "question": 'Why do we use map in react?'
+    },
+    {
+      "questionNo": 8,
+      "question": 'Why is the key prop important while rendering a list?'
+    },
+    {
+      "questionNo": 9,
+      "question": 'What is condition Rendering?'
+    },
+    {
+      "questionNo": 10,
+      "question": 'What is the difference between State and Props?'
+    },
+  
+  ]
   return (
     <div>
       <h1>Form</h1>
--- react-retest/src/pages/Home/Home.jsx
+++ react-retest/src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import Card from '../../components/Cards/Card'
 
 const Home = () => {
   return (
     <div>
-      
+      <Card />
     </div>
   )
 }
