75ee61068254909cced33a4342305986ec3d5682
yadav-adi45 <aytbkpma@gmail.com>
2026-08-03 15:42:23 +0530

    7 answers done

--- react-retest/src/pages/Form/Form.jsx
+++ react-retest/src/pages/Form/Form.jsx
@@ -2,35 +2,42 @@ const Form = () => {
   const Questions = [
     {
       "questionNo": 1,
-      "question": 'What is React?'
+      "question": 'What is React?',
+      "answer": 'React is a JS library for frontend development or to make the ui of websites. It just renders the specific part which has changed'
     },
     {
       "questionNo": 2,
-      "question": 'What is JSX?'
+      "question": 'What is JSX?',
+      "answer": 'JSX stands for javascript xml. it allows the user to write html in js'
     },
     {
       "questionNo": 3,
-      "question": 'What is a Component?'
+      "question": 'What is a Component?',
+      "answer": 'a component is a reusable part which can be used again and again. eg: Navbar etc.'
     },
     {
       "questionNo": 4,
-      "question": 'What is Props?'
+      "question": 'What is Props?',
+      "answer": 'Props used to pass data from parent to child components.'
     },
     {
       "questionNo": 5,
-      "question": 'What is useState?'
+      "question": 'What is useState?',
+      "answer": 'useState is a type of react hook which stores the changing data.'
     },
     {
       "questionNo": 6,
-      "question": 'What is Purpose of React Router?'
+      "question": 'What is Purpose of React Router?',
+      "answer": 'React router is used for routing in react webpage and allows to navigate between different components or pages.'
     },
     {
       "questionNo": 7,
-      "question": 'Why do we use map in react?'
+      "question": 'Why do we use map in react?',
+      "answer": 'We use map in react to traverse  a array and return the list of elements.'
     },
     {
       "questionNo": 8,
-      "question": 'Why is the key prop important while rendering a list?'
+      "question": 'Why is the key prop important while rendering a list?',
     },
     {
       "questionNo": 9,
